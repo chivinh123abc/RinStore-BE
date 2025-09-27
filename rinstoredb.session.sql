8390ee682dd07ba302f2a883e09c1d1cf17fa13d
Ryan Luong <mail.lcvinh@gmail.com>
2025-09-27 11:06:09 +0000

    ryanLuong/update-database-add-role-small-int

--- rinstoredb.session.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (username, email, password, phone_number, status)
-VALUES (
-    'Alice',
-    'alice@example.com',
-    'pass123',
-    '0987654321',
-    1
-  ),
-  ('Bob', 'bob@example.com', 'bobpass', NULL, 0);
